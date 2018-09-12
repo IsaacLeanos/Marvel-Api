f5a761e2ce8896457e30dbf440395f274aaa9799
unknown <isaac.leanos11@gmail.com>
2018-09-12 13:09:57 -0700

    promise error

--- marvel-api/src/components/Search.js
+++ marvel-api/src/components/Search.js
@@ -15,27 +15,33 @@ class Search extends React.Component{
 
     getCharId(){
         let name=this.state.query
-        return axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
+        axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
         .then((res)=>{
             let id=res.data.data.results[0].id
-            console.log('id',id)
             this.setState({characterID:id})
+            console.log('id1',id)
+            return id
         })
         .catch((e)=>{
             let falseChar=this.state.query
             let correctedChar=falseChar.split('').slice(0, -1).join('')
             this.setState({query:correctedChar})
         })
+        .then((id)=>{
+            console.log('id2',id)
+            axios.get(`http://gateway.marvel.com/v1/public/characters?characterId=${id}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
+            .then(()=>{
+                console.log('here')
+            })
+            .catch((e)=>{console.log('err2',e)})
+            // this.getCharComics(id)
+        })
+        .catch((e)=>{console.log('err3',e)})
     }
 
-    getCharComics(){
-        // let id=this.state.characterID
-        // axios.get(`http://gateway.marvel.com/v1/public/characters?characterId=${id}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
-        // .then((res)=>{
-        //     console.log(res)
-        // })
-        console.log('here')
-    }
+    // getCharComics(id){
+    //    return axios.get(`http://gateway.marvel.com/v1/public/characters?characterId=${id}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
+    // }
 
     onChange=(e)=>{
         this.setState({query:e.target.value})
@@ -48,7 +54,6 @@ class Search extends React.Component{
 
     onKeyUp=(e)=>{
         this.getCharId()
-        .then(this.getCharComics())
     }
 
     render(){
