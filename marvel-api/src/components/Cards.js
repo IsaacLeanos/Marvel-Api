60dab754ba04b0c27fa7ef2322de5d7218ccb93e
unknown <isaac.leanos11@gmail.com>
2018-09-12 07:40:24 -0700

    be back

--- marvel-api/src/components/Cards.js
+++ marvel-api/src/components/Cards.js
@@ -7,13 +7,40 @@ class Cards extends React.Component{
 
     render(){
         return(
-        <Col>
-        <Card header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+        <div>
+
+        <Col l={3}>
+        <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
         title="Card Title"
         reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
         <p><a href="#">This is a link</a></p>
         </Card>
         </Col>
+
+        <Col l={3}>
+        <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+        title="Card Title"
+        reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+        <p><a href="#">This is a link</a></p>
+        </Card>
+        </Col>
+        <Col l={3}>
+        <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+        title="Card Title"
+        reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+        <p><a href="#">This is a link</a></p>
+        </Card>
+        </Col>
+        <Col l={3}>
+        <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+        title="Card Title"
+        reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+        <p><a href="#">This is a link</a></p>
+        </Card>
+        </Col>
+
+
+        </div>
         )
     }
 }
