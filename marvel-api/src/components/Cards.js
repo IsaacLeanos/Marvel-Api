0cb179b53c6c85e8fedb382f8cf35383c6119170
unknown <isaac.leanos11@gmail.com>
2018-09-12 12:47:44 -0700

    axios get comics

--- marvel-api/src/components/Cards.js
+++ marvel-api/src/components/Cards.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import{Col,Card,CardTitle}from'react-materialize'
+// import axios from 'axios'
 
 class Cards extends React.Component{
 
     state={}
 
-    componentDidMount(){
-        this.fetchData()
-    }
-
-    fetchData(){
-        let name='thor'
-        fetch(`http://gateway.marvel.com/v1/public/characters?name=${name}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
-        .then((res)=>{console.log(res.json())})
-    }
+    
 
     render(){
         return(
@@ -27,6 +20,13 @@ class Cards extends React.Component{
         </Card>
         </Col>
 
+        <Col l={3}>
+        <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
+        title="Card Title"
+        reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+        <p><a href="#">This is a link</a></p>
+        </Card>
+        </Col>
         <Col l={3}>
         <Card  header={<CardTitle reveal image={"img/office.jpg"} waves='light'/>}
         title="Card Title"
--- marvel-api/src/components/Search.js
+++ marvel-api/src/components/Search.js
@@ -1,20 +1,70 @@
 import React from 'react'
 import{Col,Input,Icon}from'react-materialize'
+import axios from 'axios'
 
 class Search extends React.Component{
 
     state={
+        query:'',
+        characterID:null
+    }
 
+    // componentDidMount(){
+    //     this.fetchData()
+    // }
+
+    getCharId(){
+        let name=this.state.query
+        return axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${name}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
+        .then((res)=>{
+            let id=res.data.data.results[0].id
+            console.log('id',id)
+            this.setState({characterID:id})
+        })
+        .catch((e)=>{
+            let falseChar=this.state.query
+            let correctedChar=falseChar.split('').slice(0, -1).join('')
+            this.setState({query:correctedChar})
+        })
+    }
+
+    getCharComics(){
+        // let id=this.state.characterID
+        // axios.get(`http://gateway.marvel.com/v1/public/characters?characterId=${id}&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
+        // .then((res)=>{
+        //     console.log(res)
+        // })
+        console.log('here')
+    }
+
+    onChange=(e)=>{
+        this.setState({query:e.target.value})
+        console.log(this.state.query)
+    }
+
+    submit=(e)=>{
+        e.preventDefault()
+    }
+
+    onKeyUp=(e)=>{
+        this.getCharId()
+        .then(this.getCharComics())
     }
 
     render(){
         return(
         <Col l={7} offset="l2" s={12} style={{'border':'blue solid 1px'}}>
-        <Input              placeholder="Search..."  
+        <form               onSubmit={this.submit}>
+        <Input              placeholder="Search..."
+                            name='search'
+                            value={this.state.query}
                             label="Marvel Character"
-                            style={{'width':'400px'}}>
+                            style={{'width':'400px'}}
+                            onChange={this.onChange}
+                            onKeyUp={this.onKeyUp}>
         <Icon>arrow_forward</Icon>
         </Input>
+        </form>
         </Col>
         )
     }
