f53edbbeb6e8e64459d29a4c1b2db444b917cca2
unknown <isaac.leanos11@gmail.com>
2018-09-19 16:48:20 -0700

    mostla

--- marvel-api/src/components/Cards.js
+++ marvel-api/src/components/Cards.js
@@ -6,7 +6,6 @@ class Cards extends React.Component{
 
     state={
         characterID:this.props.characterID,
-        title:null,
         results:[]
     }
 
@@ -25,15 +24,15 @@ class Cards extends React.Component{
     }
     
     componentDidMount(){
-        this.getCharComics()
         console.log('DID MOUNT')
+        this.getCharComics()
     }
 
     getCharComics=()=>{
         let id=this.state.characterID
         axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}/comics?&ts=1&apikey=7c1f96a95f1a624e70019ff7c43bd5c3&hash=dfddec6c4f447f7fe958fba16b941320`)
         .then((res)=>{
-            console.log(res.data.data.results[0].thumbnail.path)
+            // console.log(res.data.data.results[0].thumbnail.path)
             // this.setState({title:res.data.data.results[0].title})
             let results=res.data.data.results[0]
             // let result=res.data.data.results[0].thumbnail.path
@@ -49,30 +48,42 @@ class Cards extends React.Component{
         // 'http://i.annihil.us/u/prod/marvel/i/mg/7/90/5b999ce416c79'
         // 'http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73/portrait_fantastic.jpg'
 
-        const imgs=this.state.results.map((obj)=>{
-            return(
-                <li key={obj.id}>{obj.title}</li>
-            )
-        })
-}
-
-
+    }
+    
+    // {this.state.results&&<p>{this.state.results}</p>}
+    // {this.state.title&&<p>{this.state.title}</p>}
+    
     render(){
+        // const results=this.state.results
+        // const imgs=results.map((obj)=>{
+        //     let i;
+        //     for(i=0;i<5;i++){
+        //         // return(
+        //             // <Col l={12}>
+        //             // <Card  header={<CardTitle reveal image={null} waves='light'/>}
+        //             // title={obj.title}
+        //             // reveal={<p>Here is some more clicked on.</p>}>
+        //             // <p><a href={null}>This is a link</a></p>
+        //             // </Card>
+        //             // </Col>
+        //         // )
+        //         console.log('ok')
+        //     }
+        //     console.log('ok')
+        // })
         return(
-        <div>
-
+        <div >
         {this.state.characterID&&<p>{this.state.characterID}</p>}
         {this.state.results&&<p>{this.state.results}</p>}
-        {this.state.title&&<p>{this.state.title}</p>}
 
 
-        <ul>{this.imgs}</ul>
         
         
         </div>
         )
     }
 }
+// {imgs}
 
 // <Col l={3}>
 // <Card  header={<CardTitle reveal image={this.state.results} waves='light'/>}
